f75be6e71ac3cc50b59e858ef7339721d11d6a21
Seanghay Yath <seanghay.dev@gmail.com>
2023-11-06 22:25:17 +0700

    test

--- slugify.test.mjs
+++ slugify.test.mjs
@@ -1,3 +1,19 @@
-import { slugify } from './slugify.js'
+import { readFileSync } from 'node:fs';
+import { slugify, transform } from './slugify.js';
+import { test } from 'node:test';
+import { strictEqual } from 'node:assert';
 
-console.log(slugify('មិនដឹងទេ that\'s nice'))
+test('simple', () => {
+	strictEqual(slugify('មិនដឹងទេ that\'s nice'), "mindoeng-te that's nice");
+})
+
+test('fixture', () => {
+	const items = readFileSync("lexicon.tsv", 'utf8')
+		.split('\n').map(item => item.trim().split('\t'))
+		.filter(Boolean)
+
+	for (const item of items) {
+		if (!item[0] || !item[1]) continue
+		strictEqual([...transform(item[0])].join(""), item[1]);
+	}
+})
